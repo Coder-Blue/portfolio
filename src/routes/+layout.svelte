703a3cbd6166bb9a1db0b708e9b62c280475338e
Noah Trần <trananhquan1009@gmail.com>
2025-09-16 02:01:32 +0700

    :sparkles: - background styling (#22)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,3 +12,5 @@
 <main>
   {@render children?.()}
 </main>
+<div class="background-gradient absolute inset-0 -z-50 max-h-screen"></div>
+<div class="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>
