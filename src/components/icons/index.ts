430fc533f28ea8b71e0aba46ac194bd15095be1d
Noah Trần <trananhquan1009@gmail.com>
2025-09-16 12:52:20 +0700

    :sparkles: - footer UI

---
+++ src/components/icons/index.ts
@@ -0,0 +1,9 @@
+import Facebook from "./facebook.svelte";
+import Github from "./github.svelte";
+import Twitter from "./twitter.svelte";
+
+export {
+  Facebook as FacebookIcon,
+  Github as GithubIcon,
+  Twitter as XIcon,
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,4 @@
+import Footer from "./footer.svelte";
+
 export * from "./hero";
+export { Footer };
