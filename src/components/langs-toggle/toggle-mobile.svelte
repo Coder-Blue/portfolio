5c650ea4ec37f8f2179ce6d2958ec53651d8c67e
Noah Trần <trananhquan1009@gmail.com>
2025-09-17 11:21:47 +0700

    :bug: - mobile langs switcher style

--- src/components/langs-toggle/toggle-mobile.svelte
+++ src/components/langs-toggle/toggle-mobile.svelte
@@ -15,7 +15,7 @@
   const { class: className }: MobileToggleProps = $props();
 </script>
 
-<div class="flex gap-3">
+<div class="flex gap-2">
   {#each langSwitcher as { mobileLabel, locale }}
     <button
       class={cn("group text-slate-900 relative flex w-fit items-center justify-center overflow-hidden rounded-md border-2 border-slate-900 bg-slate-50 px-4 py-2 font-bold transition-transform ease-out hover:scale-105", className)}
